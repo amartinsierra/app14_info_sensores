0617a51ab62a9b57763a0a6a0b06fe306d4ac070
antonio <antonio@gmail.com>
2019-05-10 18:05:33 +0200

    manejo sensor presión

--- app/src/main/java/graficos/curso/ejercicios/app14_info_sensores/MainActivity.java
+++ app/src/main/java/graficos/curso/ejercicios/app14_info_sensores/MainActivity.java
@@ -1,9 +1,15 @@
 package graficos.curso.ejercicios.app14_info_sensores;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import modelo.InfoSensores;
 
@@ -17,5 +23,25 @@ public class MainActivity extends AppCompatActivity {
         InfoSensores info=new InfoSensores(this);
         ArrayAdapter<String> sensores=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,info.getSensores());
         lvSensores.setAdapter(sensores);
+
+        //capturar eventos en sensor de presión
+        SensorManager sm=(SensorManager)this.getSystemService(Context.SENSOR_SERVICE);
+        Sensor spresion=sm.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        sm.registerListener(
+                new SensorEventListener() {
+                    @Override
+                    public void onSensorChanged(SensorEvent sensorEvent) {
+                        float vActual=sensorEvent.values[0];
+                        if(vActual>1000){
+                            Toast.makeText(MainActivity.this,"Presión excesiva",Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onAccuracyChanged(Sensor sensor, int i) {
+
+                    }
+                }
+                , spresion, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
